bb90ed8cf713538e7938c0ea9b0f511167681516
Abdul Mannan <abdulmann1234@gmail.com>
2023-02-27 15:09:16 +0500

    Maximum & Recursive Maximum in LinkedList

---
+++ Linked List/08 Recursive.Maimum.Eement.In.LinkedList.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first = NULL;
+
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+};
+
+int RecursiveMaximum(struct Node *p)
+{
+    int x = 0;
+    if (p == 0)
+    {
+        return INT32_MIN;
+    }
+
+    x = RecursiveMaximum(p->next);
+
+    if (x > p->data)
+    {
+        return x;
+    }
+    else
+    {
+        return p->data;
+    }
+}
+
+int main(void)
+{
+    int A[] = {2, 78, 3, 67, 4};
+    create(A, 5);
+    cout << RecursiveMaximum(first);
+
+    return 0;
+}
